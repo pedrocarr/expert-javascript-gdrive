cd1c4732574895a0a440ddac0253dbe94c6ddb18
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-09-06 15:55:58 -0300

    index.js

--- classes/class01/gdrive-webapi/src/index.js
+++ classes/class01/gdrive-webapi/src/index.js
@@ -1,4 +1,4 @@
-import { fs } from 'fs'
+import fs from 'fs'
 import https from 'https'
 
 const PORT = process.env.PORT || 3000
@@ -7,3 +7,15 @@ const localHostSSL = {
   key: fs.readFileSync('./certificates/key.pem'),
   cert: fs.readFileSync('./certificates/cert.pem'),  
 }
+
+const server = https.createServer(
+  localHostSSL,
+  (req, res) => {
+    res.end('Hello world')
+  }
+)
+
+const startServer = () => {
+  const { address, port } = server.address()
+}
+
