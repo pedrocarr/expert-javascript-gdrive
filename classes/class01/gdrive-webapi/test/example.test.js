3e47727618da61f5b2359721ca84fd8a1c1567d4
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-09-06 14:58:13 -0300

    fisrt commit

---
+++ classes/class01/gdrive-webapi/test/example.test.js
@@ -0,0 +1,11 @@
+import { 
+  describe,
+  test,
+  expect
+ } from '@jest/globals'
+
+ describe('#Testing 1,2,3', () => {
+   test('#test test ', () => {
+     expect(true).toBeTruthy()
+   })
+ })
