9e5514f7ef4b51595f32bc067e5926eb72eb7766
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-09-11 19:07:53 -0300

    socket io

--- classes/class01/gdrive-webapi/src/index.js
+++ classes/class01/gdrive-webapi/src/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import https from 'https'
 import { logger } from './logger.js'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 3000
 
@@ -8,6 +9,13 @@ const localHostSSL = {
   key: fs.readFileSync('./certificates/key.pem'),
   cert: fs.readFileSync('./certificates/cert.pem'),  
 }
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: false
+  }
+})
 
 const server = https.createServer(
   localHostSSL,
