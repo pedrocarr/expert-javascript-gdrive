cf0c2542574b0693a218d88bcd66ac56f0867700
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-09-23 13:34:59 -0300

    add tests parameters

--- classes/class01/gdrive-webapi/jest.config.mjs
+++ classes/class01/gdrive-webapi/jest.config.mjs
@@ -1,18 +1,19 @@
-
-
 export default {
-
-
-
   clearMocks: true,
-
   collectCoverage: true,
-
-
   coverageDirectory: "coverage",
-
-
   coverageProvider: "v8",
-
-
+  coverageReporters: [
+    "text",
+    "lcov"
+  ],
+  testEnvironment: "node",
+  coverageThreshould: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 };
