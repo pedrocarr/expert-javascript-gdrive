74034252f428fbbea6c16c46528b11799bdaebfb
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-10-05 16:04:03 -0300

    making tests

--- classes/class01/gdrive-webapi/test/unit/routes.test.js
+++ classes/class01/gdrive-webapi/test/unit/routes.test.js
@@ -3,8 +3,23 @@ import {
   test,
   expect
  } from '@jest/globals'
+import Routes from '../../src/routes'
 
  describe('#Routes test suite', () => {
+
+   describe('#setSocketInstance', () => {
+      test('setSocket should store io instance', () => {
+        const routes = new Routes()
+        const ioObj = {
+          to: (id) => ioObj,
+          emit: (event, message) => {}
+        }
+
+        // routes.setSocketInstance(ioObj)
+        expect(routes.io).toStrictEqual(ioObj)
+
+      })
+   })
    test('#test test ', () => {
      expect(true).toBeTruthy()
    })
